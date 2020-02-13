69d9645192c2943cd13632dc78e941cff69ca292
Kayla Reyes <kay@Kaylas-Air.lan>
2020-02-12 20:45:23 -0600

    completed examples/03_module/04_for_ranged

--- src/examples/03_module/04_for_ranged/for_ranged.cpp
+++ src/examples/03_module/04_for_ranged/for_ranged.cpp
@@ -1,29 +1,27 @@
 #include "for_ranged.h"
 #include<iostream>
 #include<vector>
-
-using std::vector; using std::cout;
+using std::vector;
+using std::cout;
 
 /*
 Write code for loop_string_w_index that accepts a string parameter.
 The function uses an indexed for loop to iterate and display the characters in the
 string as follows:
 for string test displays
-
 t
 e
 s
 t
 */
-void loop_string_w_index(const string &str)
 
-{ 
-	for (int i=0; i < str.length(); ++i)
+void loop_string_w_index(const string & str)
+{
+	for (int i = 0; i < str.length(); i++)
 	{
-		cout<<str[i]<< "\n";
+		cout << str[i] << "\n";
 	}
 }
-
 
 
 /*
@@ -31,13 +29,12 @@ Write code for loop_string_w_index that accepts a string parameter.
 The function uses a for ranged loop using auto to iterate and display the characters in the
 string as follows:
 for string test displays
-
-
 t
 e
 s
 t
 */
+
 void loop_string_w_auto(const string & str)
 {
 	for (auto s : str)
@@ -45,6 +42,7 @@ void loop_string_w_auto(const string & str)
 		cout << s << "\n";
 	}
 }
+
 
 void loop_vector_w_index() 
 {
@@ -55,4 +53,3 @@ void loop_vector_w_index()
 		std::cout << nums[i] << "\n";
 	}
 }
-
--- src/examples/03_module/04_for_ranged/for_ranged.h
+++ src/examples/03_module/04_for_ranged/for_ranged.h
@@ -1,12 +1,11 @@
 #include<string>
 #include<vector>
 
-using std::string; using std::cout;
 using std::string;
+
 //write a void function prototype for loop_string_w_index that accepts a string parameter
 void loop_string_w_index(const string &str);
 
 
 //write a void function prototype for loop_string_w_auto  that accepts a string parameter
-
-
+void loop_string_w_auto(const string &str);
