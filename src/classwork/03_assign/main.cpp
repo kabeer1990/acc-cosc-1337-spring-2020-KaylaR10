9091b51c319e17979385a175f34a2474224465c3
Kayla Reyes <kay@Kaylas-MacBook-Air.local>
2020-02-12 19:06:02 -0600

    loops assign 03

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,15 +1,23 @@
-#write includes statements
-
+//write includes statements
+#include "loops.h"
+#include <iostream>
 //write using statements for cin and cout
-
+using std::cout; using std::cin;
 
 /*
 Use a do while loop to prompt the user for 
 a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
-int main() 
-{
-
-	return 0;
+int main() {
+int num;
+do {
+	cout << "Enter a number (negative number to exit):";
+	cin >> num;
+	if (num >=0) {
+		cout << "Factorial of " << num << "is" << factorial(num) << "\n";
+	}
+} while (num >=0);
+return 0;
 }
+
