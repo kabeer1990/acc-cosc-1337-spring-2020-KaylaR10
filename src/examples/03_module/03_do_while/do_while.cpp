4e01e329c08ad71cfa1583586147077cbb8c6078
Kayla Reyes <kay@Kaylas-Air.lan>
2020-02-12 20:41:52 -0600

    completed example/03_module/03_do_while

--- src/examples/03_module/03_do_while/do_while.cpp
+++ src/examples/03_module/03_do_while/do_while.cpp
@@ -1,12 +1,27 @@
 #include<iostream>
 #include "do_while.h"
-#include "switch.h"
+#include "while.h"
 
 using std::cout;
 using std::cin;
 
 //Write code for void function prompt_user to loop until
 //user opts not to continue.  
+void prompt_user()
+{
+	int choice;
+	do 
+	{
+		int num;
+		cout << "Enter a number: ";
+		cin >> num;
+		cout << sum_of_squares(num) << "\n\n";
+
+		cout << "Continue?... (0: No, 1: Yes)";
+		cin >> choice;
+
+	} while (choice == 1);
+}
 
 
 //Write code for function run_menu that prompts  user for a 
@@ -14,6 +29,4 @@ using std::cin;
 /*
 Use the existing menu_option function from /example/02_module/03_switch
 folder.
-
 */
-
