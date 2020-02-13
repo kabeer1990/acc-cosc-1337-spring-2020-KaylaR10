78ae3044d67c36cf5dd37167d26d3175a6d4c3a7
Kayla Reyes <kay@Kaylas-Air.lan>
2020-02-12 20:49:32 -0600

    Completed 03_module tests

--- test/examples_test/03_module_test/03_module_tests.cpp
+++ test/examples_test/03_module_test/03_module_tests.cpp
@@ -5,3 +5,29 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+
+#define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
+#include "catch.hpp"
+#include "while.h"
+#include "value_ref.h"
+
+TEST_CASE("Verify Test Configuration", "verification") {
+	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify sum_of_squares function")
+{
+	REQUIRE(sum_of_squares(3) == 14);
+	REQUIRE(sum_of_squares(4) == 30);
+	REQUIRE(sum_of_squares(5) == 55);
+}
+
+TEST_CASE("Test pass_by_value_and_ref function")
+{
+	int num1 = 5, num2 = 10;
+
+	pass_by_val_and_ref(num1, num2);
+
+	REQUIRE(num1 == 5);
+	REQUIRE(num2 == 50);
+}
